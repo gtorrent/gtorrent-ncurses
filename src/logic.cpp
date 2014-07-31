80fffb2b22b400edb1354f9461e6612807fa5ed6
ragesalmon <ragesalmon@gmail.com>
2014-07-30 23:23:03 -0400

    Got layout back, going to merge to core in a second

--- src/logic.cpp
+++ src/logic.cpp
@@ -8,9 +8,21 @@ int main_loop() {
     shared_ptr<gt::Core> gt_core(new gt::Core);
 
     if(gt_core->isRunning()) {
+        infobar->printw("Running");
+        infobar->refresh();
+    }
+
+    while(1) {
+        char chr = getch();
+        parent->refresh();
+        if(chr == 'q') {
+            break;
+        }
     }
 
     gt_core->shutdown();
+
+    endwin();
     return 1;
 }
 
