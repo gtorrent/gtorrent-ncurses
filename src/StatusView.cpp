8eaa12eb64f4441737966789145e791ca63004c4
IGI-111 <igi.222@gmail.com>
2014-08-03 17:57:24 +0200

    fix for StatusView panel

--- src/StatusView.cpp
+++ src/StatusView.cpp
@@ -1,30 +1,26 @@
 #include "StatusView.hpp"
 #include "Application.hpp"
 #include <memory>
-#include <iostream> //FIXME: debug
 
-StatusList::StatusList() {
-    update();
-}
-
-void StatusList::update() {
+unsigned StatusView::getStatusCount(const libtorrent::torrent_status::state_t &status)
+{
+    unsigned i = 0;
     std::vector<std::shared_ptr<gt::Torrent>> torrents = Application::getSingleton()->getCore()->getTorrents();
-    for(unsigned i = 0; i < torrents.size(); i++) {
-        if(torrents[i]->getState() == libtorrent::torrent_status::state_t::downloading) {
-            t_downloading.push_back(torrents[i]);
-        } else if (torrents[i]->getState() == libtorrent::torrent_status::state_t::checking_files) {
-            t_checking.push_back(torrents[i]);
-        } else if (torrents[i]->getState() == libtorrent::torrent_status::state_t::finished) {
-            t_completed.push_back(torrents[i]);
-        } else if (torrents[i]->getState() == libtorrent::torrent_status::state_t::seeding) {
-            t_seeding.push_back(torrents[i]);
-        } else {
-            t_stopped.push_back(torrents[i]);
-        }
-    }
+    for(auto t : torrents)
+        if(t->getState() == status)
+            ++i;
+    return i;
 }
 
-StatusList::~StatusList() {}
+unsigned StatusView::getPausedCount()
+{
+    unsigned i = 0;
+    std::vector<std::shared_ptr<gt::Torrent>> torrents = Application::getSingleton()->getCore()->getTorrents();
+    for(auto t : torrents)
+        if(t->getHandle().status().paused)
+            ++i;
+    return i;
+}
 
 StatusView::StatusView(int nlines, int ncols, int begin_y, int begin_x)
     : NCursesPanel(nlines, ncols, begin_y, begin_x)
@@ -34,13 +30,12 @@ StatusView::StatusView(int nlines, int ncols, int begin_y, int begin_x)
 
 void StatusView::update()
 {
-    statlist.update();
     printw(1, 2, "All (%zu)", Application::getSingleton()->getCore()->getTorrents().size());
-    printw(2, 2, "Downloading (%zu)", statlist.t_downloading.size());
-    printw(3, 2, "Seeding (%zu)", statlist.t_seeding.size());
-    printw(4, 2, "Checking (%zu)", statlist.t_checking.size());
-    printw(5, 2, "Completed (%zu)", statlist.t_completed.size());
-    printw(6, 2, "Stopped (%zu)", statlist.t_stopped.size());
+    printw(2, 2, "Downloading (%u)", getStatusCount(libtorrent::torrent_status::state_t::downloading));
+    printw(3, 2, "Seeding (%u)", getStatusCount(libtorrent::torrent_status::state_t::seeding));
+    printw(4, 2, "Checking (%u)", getStatusCount(libtorrent::torrent_status::state_t::checking_files));
+    printw(5, 2, "Completed (%u)", getStatusCount(libtorrent::torrent_status::state_t::finished));
+    printw(6, 2, "Paused (%u)", getPausedCount());
     refresh();
 }
 
